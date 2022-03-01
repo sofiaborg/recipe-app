1e36e298ab6dbd2bf54b6a9bb2dfe0504b31eea8
Sofia Borg <fia.borg@hotmail.com>
2022-03-01 15:43:09 +0100

    lade till models

---
+++ models/RecipeModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+    recipeTitle: { type: String, required=true },
+    recipeTime: { type: Number, required=true },
+    recipeDescription: { type: String, required=true },
+    
+});
+
+const RecipeModel = mongoose.model("recipe", recipeSchema);
+
+module.exports = RecipeModel;
---
+++ models/RegisterModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const registerSchema = new mongoose.Schema({
+    registerFirstname: { type: String, required=true },
+    registerLastname: { type: String, required=true },
+    registerEmail: { type: String, required=true },
+    registerUsername: { type: String, required=true },
+    registerPassword: { type: Number, required=true },
+    registerConfirm: { type: String, required=true },
+});
+
+const registerModel = mongoose.model("register", registerSchema);
+
+module.exports = registerModel;
