fd12a6c489d7cb6cff77c16174ba63a9b0b3d870
Sofia Borg <fia.borg@hotmail.com>
2022-03-02 14:40:38 +0100

    added ext for win and mac when compiling

--- routes/reviews-routes.js
+++ routes/reviews-routes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const utils = require('../utils.js');
-const mongoose = require('mongoose');
+const express = require("express");
+const utils = require("../utils.js");
+const mongoose = require("mongoose");
 
 // const ReviewModel = require('./models/RewievModel.js');
 
 const router = express.Router();
 
 //GET - my reviews
-router.get('/my-reviews', (req,res) => {
-    res.render('reviews/my-reviews-list')
-})
+router.get("/my-reviews", (req, res) => {
+  res.render("reviews/my-reviews-list");
+});
 
 module.exports = router;
