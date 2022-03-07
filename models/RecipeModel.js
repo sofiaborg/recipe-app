b425d7e65b4d49631e6ed1cd5e5723059a9276c8
Sofia Borg <fia.borg@hotmail.com>
2022-03-07 15:11:54 +0100

    ok

--- models/RecipeModel.js
+++ models/RecipeModel.js
@@ -5,6 +5,7 @@ const RecipeSchema = new Schema({
   recipeTime: { type: Number, required: true },
   recipeDescription: { type: String, required: true },
   createdByUser: { type: Schema.Types.ObjectId, ref: "users", required: true },
+  reviewed: [{ type: Schema.Types.ObjectId, ref: "reviews", required: true }],
 });
 
 const RecipeModel = model("recipes", RecipeSchema);
--- models/ReviewModel.js
+++ models/ReviewModel.js
@@ -4,13 +4,8 @@ const reviewSchema = new Schema({
   reviewDescription: { type: String, required: true },
   reviewStars: { type: Number, required: true },
   reviewDate: { type: Number, default: Date.now },
-  // postBy: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "Recipe",
-  //   required: true,
-  // },
 });
 
-const ReviewModel = model("Review", reviewSchema);
+const ReviewModel = model("reviews", reviewSchema);
 
 module.exports = ReviewModel;
